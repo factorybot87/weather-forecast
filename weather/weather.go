66abd0d1ba1cea8226a00ec30abaaf3288015e1b
jerry guo <jerry.kuo@g.ncu.edu.tw>
2022-01-06 04:53:34 +0800

    implement ParseResponseBody

--- weather/weather.go
+++ weather/weather.go
@@ -1,6 +1,7 @@
 package weather
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -25,4 +26,36 @@ func BuildRequest(url string) *http.Request {
 func GetBody(res *http.Response) string {
 	body, _ := io.ReadAll(res.Body)
 	return string(body)
+}
+
+// ParseResponseBody will parse an API reponse into a Forecast
+func ParseResponseBody(jsonData string) (Forecast, error) {
+	forecast := Forecast{}
+	err := json.Unmarshal([]byte(jsonData), &forecast)
+	return forecast, err
+}
+
+// Forecast has only the parts we care about, currentConditions and Days
+type Forecast struct {
+	CurrentConditions CurrentCondition
+	Days              []DailyWeather
+}
+
+// CurrentCondition represent the current weather
+type CurrentCondition struct {
+	DatetimeEpoch int64
+	Temp          float32
+	WindSpeed     float32
+	Conditions    string
+}
+
+// DailyWeather represent the daily forecast for the next 15 days
+type DailyWeather struct {
+	DatetimeEpoch int64
+	Temp          float32
+	TempMin       float32
+	TempMax       float32
+	WindSpeed     float32
+	Conditions    string
+	Description   string
 }
