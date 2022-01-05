ebfce40a6159919e65ba66cacc06647b1fd040f7
jerry guo <jerry.kuo@g.ncu.edu.tw>
2022-01-06 00:41:24 +0800

    test with example

--- weather/weather_test.go
+++ weather/weather_test.go
@@ -1,13 +1,28 @@
 package weather_test
 
 import (
+	"fmt"
 	"forecast/weather"
 	"testing"
 )
 
 func TestGenerateURL(t *testing.T) {
 
-	if weather.GenerateURL() != "url" {
-		t.Fatal("wrong")
+	location := "Taipei"
+	key := "dummyKey"
+
+	want := "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Taipei?key=dummyKey"
+	got := weather.GenerateURL(location, key)
+
+	assertEqual(want, got, t)
+}
+
+func assertEqual(want, got string, t *testing.T) {
+	if got != want {
+		t.Fatal(outputMessage(want, got))
 	}
+}
+
+func outputMessage(want, got string) string {
+	return fmt.Sprint("\nexpect:\n", want, "\n\n got:\n", got)
 }
