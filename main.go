9cb6b2f79f3eb4426a9f4f98c12608f36bb3114b
jerry guo <jerry.kuo@g.ncu.edu.tw>
2022-01-06 16:24:16 +0800

    get forecast from API

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"forecast/weather"
+	"net/http"
+)
+
 const (
 	key      = ""
 	location = "taipei"
@@ -7,5 +12,24 @@ const (
 )
 
 func main() {
+	forecast, _ := getForecast(location, key)
+	formatOutput(forecast)
+}
+
+func getForecast(location, key string) (weather.Forecast, error) {
+	client := http.Client{}
+	url := weather.GenerateURL(location, key)
+	req := weather.BuildRequest(url)
+
+	res, _ := client.Do(req)
+	// TODO: Check reponse status code
+
+	jsonData := weather.GetBody(res)
+	forecast, err := weather.ParseResponseBody(jsonData)
+
+	return forecast, err
+}
+
+func formatOutput(forecast weather.Forecast) {
 
 }
